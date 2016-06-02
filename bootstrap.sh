71e2b60f4ec6550c7490e121dee20fe354024a2f
Guangliang Zhao <lucienchao@gmail.com>
2016-06-02 10:37:39 -0700

    bugfix: bootstrap.sh
    Signed-off-by: Guangliang Zhao <lucienchao@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,16 +26,20 @@ declare -a packager
 #platform independence packages
 declare -a packages_pip
 
-repo_dir=`dirname $0`
+repo_dir=$(cd `dirname $0`; pwd)
 
 function usage()
 {
-	echo "-d	install with desktop related packages."
-	echo "-m	install with mail related packages, such as mutt, offlinemap etc."
-	echo "-p	install with python related packages and python modules"
-	echo "-r	install with rust related packages(default)"
+	echo "bootstrap.sh"
+	echo "-d	default action, install base, rust and vim"
+	echo "-b	install base tool packages and links. git tmux etc"
+	echo "-v	install vim related packages and links. vim and plugins(YCM and others)"
+	echo "-D	install desktop related packages"
+	echo "-m	install mail related packages, such as mutt, offlinemap etc."
+	echo "-p	install python related packages and python modules"
+	echo "-r	install rust related packages"
 	echo "-s	restore all installed links"
-	echo "-h
+	echo "-h"
 }
 
 function update_packagers_by_platform()
@@ -71,8 +75,12 @@ function setup_links()
 {
 	links=${@:1}
 
-	for i in ${ipset[@]}; do
-		mv ~/.${i#_}{,.restore}
+	for i in ${links[@]}; do
+		if [[ -f ~/.${i#_} ]]; then
+			echo "mv -f"  ~/.${i#_}{,.restore}
+			mv -f ~/.${i#_}{,.restore}
+		fi
+		echo "link" ${repo_dir}/$i ~/.${i#_}
 		ln -s ${repo_dir}/$i ~/.${i#_}
 	done
 }
@@ -81,73 +89,123 @@ function restore_links()
 {
 	links=${@:1}
 
-	for i in ${ipset[@]}; do
+	for i in ${links[@]}; do
 		if [[ -f ~/.${i#_}.restore ]]; then
-			mv ~/.${i#_}{.restore,}
+			echo "mv -f" ~/.${i#_}{.restore,}
+			mv -f ~/.${i#_}{.restore,}
 		fi
 	done
 }
 
 function setup_vim_plugins()
 {
+	echo "Installing packages for YouCompleteMe" ${ycm_packages[*]}
+	sudo ${packager}  ${ycm_packages[*]}
+
 	vim -u $repo_dir/_vimrc +PluginInstall +qall
 	#for c-family, golang and rust
 	cd ~/.vim/bundle/YouCompleteMe/ && git submodule update --init --recursive \
 		&& ./install.sh --clang-completer --gocode-completer --racer-completer
 }
 
-function main()
+function setup_base()
 {
-	update_packagers_by_platform
-
 	echo "Installing" ${common_packages[*]}
 	sudo ${packager}  ${common_packages[*]}
 
 	echo "Installing platform independence packages" ${packages_pip[*]}
 	sudo ${packager} ${packages_pip[*]}
 
-	echo "Installing packages for YouCompleteMe" ${ycm_packages[*]}
-	sudo ${packager}  ${ycm_packages[*]}
+	setup_links ${common_links[*]}
+}
 
+function setup_desktop()
+{
+        echo "Installing desktop related packages"
+	sudo ${packager}  ${desktop_packages[*]}
+}
+
+function setup_rust()
+{
 	echo "Installing rust related packages"
 	sudo ${packager}  ${rust_packages[*]}
+}
 
-	setup_links $common_links $vim_links
+function setup_python()
+{
+	echo "Installing python packages" ${python_packages[*]}
+	sudo ${packager}  ${python_packages[*]}
 
-	while getopts "dmprs" arg
+	echo "Installing python modules" ${python_modules[*]}
+	sudo pip install -q ${python_modules[*]}
+}
+
+function setup_vim()
+{
+	setup_links ${vim_links[*]}
+	setup_vim_plugins
+}
+
+function setup_mail()
+{
+	echo "Installing mail related packages"
+	sudo ${packager}  ${mail_packages[*]}
+
+	#TODO setup mail servers and password etc.
+	setup_links ${mail_links[*]}
+}
+
+function restore_all()
+{
+	echo "restore all old files"
+	restore_links ${common_links[*]} ${vim_links[*]} ${mail_links[*]}
+}
+
+function main()
+{
+	update_packagers_by_platform
+
+	while getopts "dbvDmprsh" arg
 	do
 		case $arg in
 		     d)
-		        echo "Installing desktop related packages"
-			sudo ${packager}  ${desktop_packages[*]}
+			setup_base
+			setup_rust
+			setup_vim
+			;;
+		     b)
+			setup_base
+			;;
+		     v)
+			# must setup base first
+			setup_base
+			setup_vim
+			;;
+		     D)
+			setup_desktop
 			;;
 		     m)
-			setup $mail_links
-			#TODO setup mail servers and password etc.
-		        echo "Installing mail related packages"
-			sudo ${packager}  ${mail_packages[*]}
+			setup_mail
 			;;
 		     p)
-			echo "Installing python packages" ${python_packages[*]}
-			sudo ${packager}  ${python_packages[*]}
-
-			echo "Installing python modules" ${python_modules[*]}
-			sudo pip install -q ${python_modules[*]}
+			setup_python
 			;;
 		     r)
-		        echo "Installing rust related packages"
-			sudo ${packager}  ${rust_packages[*]}
+			setup_rust
 			;;
 		     s)
-		        echo "restore all old files"
-			restore_links $common_links $vim_links $mail_links
+			restore_all
+			;;
+		     h)
+			usage
 			;;
 		     ?)
-		        echo "unkonw argument"
-			usage()
-			exit 1 ;;
+		        echo "unknow argument"
+			usage
+			exit 1
+			;;
 		esac
 	done
-
-	setup_vim_plugins()
 }
+
+main $@
