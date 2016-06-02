8b81dbb9dd0a248317cfc82e081c338368c51eea
Guangliang Zhao <lucienchao@gmail.com>
2016-06-02 00:38:22 -0700

    deploy with bootstrap.sh
    Signed-off-by: Guangliang Zhao <lucienchao@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,61 +1,153 @@
 #!/bin/bash
 set -e
 
-# another software: chrome, sogou pingying, jinshan kuaipan
+common_packages=('git' 'vim' 'cscope' 'cgvg' 'subversion'		\
+		'tmux' 'irssi' 'gnupg' 'openssh-server')
 
-packages_common=('git' 'vim' 'cscope' 'exuberant-ctags' 'ipython'	\
-		'cgvg'							\
-		'tmux' 'irssi' 'gnupg' 'openssh-server'			\
-		'msmtp' 'offlineimap' 'mutt' 'getmail4'			\ 
-		'python-dev' 'cmake' 'build-essential')
+mail_packages=('msmtp' 'offlineimap' 'mutt' 'getmail')
+# another software: chrome, sogou pingying
+desktop_packages=('retext' 'goldendict' 'mplayer' 'unrar'		\
+		  'w3m' 'sysstat' 'pidgin' 'nautilus-dropbox')
 
-python_modules=('pep8' 'keyring' 'python-crontab')
+python_packages=('python-dev' 'python-pip' 'ipython')
+python_modules=('pep8' 'keyring')
 
-packages_desktop=('retext' 'goldendict' 'mplayer' 'unrar'		\
-		  'w3m' 'sysstat''pidgin''nautilus-dropbox')
+rust_packages=('rustc' 'cargo')
 
-if command -v lsb_release >/dev/null 2>&1; then
-	case "$(lsb_release --id --short)" in
-		Ubuntu|Debian)
-		packager="apt-get install -y -q"
-		packages_debian=('sl' 'cowsay' 'ack-grep')
-		;;
-	esac
+ycm_packages=('cmake' 'build-essential' 'python-dev' 'python3-dev')
 
-	case "$(lsb_release --id --short | awk '{print $1}')" in
-		openSUSE|SUSE)
-		packager="zypper install"
-		packages_suse=()
-		;;
-	esac
+common_links=('_alias' '_bashrc' '_tmux.conf' '_irssi' '_bin' 		\
+		'_git-completion.bash' '_gitconfig' '_git-prompt.sh')
 
-else
-	if [ -f /etc/redhat-release ]; then
-		case "$(cat /etc/redhat-release | awk '{print $1}')" in
-			CentOS|Red)
-			packager="yum install -y -q"
-			packages_redhat=()
+vim_links=('_vimrc' '_ycm_extra_conf.py')
+mail_links=('_getmail' '_msmtprc' '_offlineimaprc' '_mutt')
+
+declare -a packager
+#platform independence packages
+declare -a packages_pip
+
+repo_dir=`dirname $0`
+
+function usage()
+{
+	echo "-d	install with desktop related packages."
+	echo "-m	install with mail related packages, such as mutt, offlinemap etc."
+	echo "-p	install with python related packages and python modules"
+	echo "-r	install with rust related packages(default)"
+	echo "-s	restore all installed links"
+	echo "-h
+}
+
+function update_packagers_by_platform()
+{
+	if command -v lsb_release >/dev/null 2>&1; then
+		case "$(lsb_release --id --short)" in
+			Ubuntu|Debian)
+			packager="apt-get install -y -q"
+			packages_pip=('sl' 'cowsay' 'ack-grep' 'exuberant-ctags')
 			;;
 		esac
+
+		case "$(lsb_release --id --short | awk '{print $1}')" in
+			openSUSE|SUSE)
+			packager="zypper install"
+			packages_pip=()
+			;;
+		esac
+
+	else
+		if [ -f /etc/redhat-release ]; then
+			case "$(cat /etc/redhat-release | awk '{print $1}')" in
+				CentOS|Red)
+				packager="yum install -y -q"
+				packages_pip=()
+				;;
+			esac
+		fi
 	fi
-fi
+}
 
-echo "Installing" ${packages_common[*]}
-sudo ${packager}  ${packages_common[*]}
+function setup_links()
+{
+	links=${@:1}
 
-echo "Installing" ${packages_debian[*]}
-sudo ${packager} ${packages_debian[*]}
+	for i in ${ipset[@]}; do
+		mv ~/.${i#_}{,.restore}
+		ln -s ${repo_dir}/$i ~/.${i#_}
+	done
+}
 
-echo "Installing" ${packages_suse[*]}
-sudo ${packager} ${packages_suse[*]}
+function restore_links()
+{
+	links=${@:1}
 
-echo "Installing" ${packages_redhat[*]}
-sudo ${packager} ${packages_redhat[*]}
+	for i in ${ipset[@]}; do
+		if [[ -f ~/.${i#_}.restore ]]; then
+			mv ~/.${i#_}{.restore,}
+		fi
+	done
+}
 
-sudo easy_install -q pip
-echo "Installing python modules" ${python_modules[*]}
-sudo pip install -q ${python_modules[*]}
+function setup_vim_plugins()
+{
+	vim -u $repo_dir/_vimrc +PluginInstall +qall
+	#for c-family, golang and rust
+	cd ~/.vim/bundle/YouCompleteMe/ && git submodule update --init --recursive \
+		&& ./install.sh --clang-completer --gocode-completer --racer-completer
+}
 
-#test -s ~/repos/ || mkdir ~/repos/
-#git clone git@github.com:glzhao/config.git ~/repos/
-#python ~/repos/config/setup.py -t all
+function main()
+{
+	update_packagers_by_platform
+
+	echo "Installing" ${common_packages[*]}
+	sudo ${packager}  ${common_packages[*]}
+
+	echo "Installing platform independence packages" ${packages_pip[*]}
+	sudo ${packager} ${packages_pip[*]}
+
+	echo "Installing packages for YouCompleteMe" ${ycm_packages[*]}
+	sudo ${packager}  ${ycm_packages[*]}
+
+	echo "Installing rust related packages"
+	sudo ${packager}  ${rust_packages[*]}
+
+	setup_links $common_links $vim_links
+
+	while getopts "dmprs" arg
+	do
+		case $arg in
+		     d)
+		        echo "Installing desktop related packages"
+			sudo ${packager}  ${desktop_packages[*]}
+			;;
+		     m)
+			setup $mail_links
+			#TODO setup mail servers and password etc.
+		        echo "Installing mail related packages"
+			sudo ${packager}  ${mail_packages[*]}
+			;;
+		     p)
+			echo "Installing python packages" ${python_packages[*]}
+			sudo ${packager}  ${python_packages[*]}
+
+			echo "Installing python modules" ${python_modules[*]}
+			sudo pip install -q ${python_modules[*]}
+			;;
+		     r)
+		        echo "Installing rust related packages"
+			sudo ${packager}  ${rust_packages[*]}
+			;;
+		     s)
+		        echo "restore all old files"
+			restore_links $common_links $vim_links $mail_links
+			;;
+		     ?)
+		        echo "unkonw argument"
+			usage()
+			exit 1 ;;
+		esac
+	done
+
+	setup_vim_plugins()
+}
