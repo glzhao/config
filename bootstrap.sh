f2d815ebd22ad310ee64db976c9188300436488e
Guangliang Zhao <lucienchao@gmail.com>
2017-10-15 13:55:56 +0800

    Revert "add submodule spf13-vim"
    This reverts commit 8bd0dca0f346b02e83d7366253bab7405f460288.

--- bootstrap.sh
+++ bootstrap.sh
@@ -111,17 +111,14 @@ function setup_vim_plugins()
 		&& ./install.sh --clang-completer --gocode-completer --racer-completer
 }
 
-function setup_packages()
+function setup_base()
 {
 	echo "Installing" ${common_packages[*]}
 	sudo ${packager}  ${common_packages[*]}
 
 	echo "Installing platform independence packages" ${packages_pip[*]}
 	sudo ${packager} ${packages_pip[*]}
-}
 
-function setup_base()
-{
 	setup_links ${common_links[*]}
 }
 
@@ -183,21 +180,16 @@ function main()
 	do
 		case $arg in
 		     d)
-			setup_packages
 			setup_base
 			setup_rust
 			setup_vim
 			setup_zsh
-			;;
-		     P)
-			setup_packages
 			;;
 		     b)
 			setup_base
 			;;
 		     v)
 			# must setup base first
-			setup_packages
 			setup_base
 			setup_vim
 			;;
