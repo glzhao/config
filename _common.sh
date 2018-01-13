b08153985e1b8304d2762a89ee3c47dc926b9661
Guangliang Zhao <lucienchao@gmail.com>
2018-01-13 12:13:44 +0800

    add fasd and it's alias

--- _common.sh
+++ _common.sh
@@ -1,5 +1,8 @@
 export PATH=$HOME/.bin:$PATH:/sbin:/usr/sbin
+export MANPATH=$HOME/.bin/man:$MANPATH
 export EDITOR=$(which vim)
 export CHEATCOLORS=true
+
+eval "$(fasd --init auto)"
 
 test -s ~/.alias && source ~/.alias || true
